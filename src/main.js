effd62af8a9a3da4ba010fd8e37cb61d99d5e76e
Abdullokh_dev <abdulloh3525Dev@gmail.com>
2023-06-30 11:03:47 +0300

    enable (router, bootstrap)

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import router from './plugins/router.js'
+import 'bootstrap/dist/css/bootstrap.css'
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
 
-createApp(App).mount('#app')
+createApp(App).use(router, bootstrap).mount('#app')
+
