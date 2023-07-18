aff846e4868a4fced55e86f6d27923e2a6fdadd9
Abdullokh_dev <abdulloh3525Dev@gmail.com>
2023-07-18 11:52:40 +0300

    Fix Urls

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -3,6 +3,7 @@ import Home from "../pages/Home.vue"
 import HomePod from "../pages/HomePod.vue"
 import Nozzles from "../pages/Nozzles.vue"
 import YandexStation from "../pages/YandexStation.vue"
+import GamePads from "../pages/GamePads.vue"
 
 const router = createRouter({
   history: createWebHashHistory(),
@@ -11,19 +12,32 @@ const router = createRouter({
   routes: [
     {
       path: '/',
+      name: Home,
       component: Home
     },
     {
-      path: '/detail',
+      path: '/home-pod',
+      name: HomePod,
       component: HomePod
     },
     {
       path: '/nozzles',
+      name: Nozzles,
       component: Nozzles
     },
     {
       path: '/yandex-station',
+      name: YandexStation,
       component: YandexStation
+    },
+    {
+      path: '/gamepads',
+      name: GamePads,
+      component: GamePads
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      component: Home
     }
   ]
 })
